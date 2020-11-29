a926cc82bdbd8cdec9afb072e538f077b7cca3bb
qnaiv <43468715+qnaiv@users.noreply.github.com>
2020-11-29 13:50:42 +0000

    typescript化

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,8 +8,8 @@
           <v-list-item v-for="todo in todos" :key="todo.id">
             <v-list-item-content>
               <v-list-item-title
-                v-text="todo.title"
                 class="todo-title"
+                v-text="todo.title"
               ></v-list-item-title>
 
               <v-list-item-subtitle>
@@ -45,43 +45,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue, Component, Prop, Watch, Emit, Ref } from 'vue-property-decorator'
 import { v4 as uuidv4 } from 'uuid'
 
-export interface IWindow extends Window {
-  webkitSpeechRecognition: any
-}
+@Component
+export default class Index extends Vue {
+  isRecording: boolean = false
+  todos: Array<Todo> = []
 
-class Todo {
-  constructor(title: string) {
-    this.id = uuidv4()
-    this.title = title
-    this.isDone = false
-    this.createdAt = new Date()
-  }
-  id: string
-  title: string
-  isDone: boolean
-  createdAt: Date
-  doneAt: Date
-}
-
-export default Vue.extend({
-  data() {
-    return {
-      isRecording: false,
-      todos: [],
-    }
-  },
-  computed: {
-    recButtonColor() {
-      return this.isRecording ? 'pink' : 'gray'
-    },
-    webkitSpeechRecognition() {
-      const { webkitSpeechRecognition }: IWindow = <IWindow>(<unknown>window)
-      return new webkitSpeechRecognition()
-    },
-  },
   created() {
     const rec = this.webkitSpeechRecognition
     rec.continuous = false // 後述
@@ -102,22 +73,103 @@ export default Vue.extend({
     rec.onaudioend = () => {
       this.isRecording = false
     }
-  },
-  methods: {
-    recognizeVoice() {
-      const rec = this.webkitSpeechRecognition
-      if (this.isRecording) {
-        this.isRecording = false
-        rec.stop()
-      } else {
-        rec.start() // 認識を開始します
-      }
-    },
-    removeTodo(todoId: string) {
-      this.todos = this.todos.filter((t: Todo) => t.id !== todoId)
-    },
-  },
-})
+  }
+
+  get recButtonColor() {
+    return this.isRecording ? 'pink' : 'gray'
+  }
+  get webkitSpeechRecognition() {
+    const { webkitSpeechRecognition }: IWindow = <IWindow>(<unknown>window)
+    return new webkitSpeechRecognition()
+  }
+
+  recognizeVoice() {
+    const rec = this.webkitSpeechRecognition
+    if (this.isRecording) {
+      this.isRecording = false
+      rec.stop()
+    } else {
+      rec.start() // 認識を開始します
+    }
+  }
+  removeTodo(todoId: string) {
+    this.todos = this.todos.filter((t: Todo) => t.id !== todoId)
+  }
+}
+
+export interface IWindow extends Window {
+  webkitSpeechRecognition: any
+  SpeechRecognition: any
+}
+
+class Todo {
+  constructor(title: string) {
+    this.id = uuidv4()
+    this.title = title
+    this.isDone = false
+    this.createdAt = new Date()
+    this.doneAt = null
+  }
+
+  id: string
+  title: string
+  isDone: boolean
+  createdAt: Date
+  doneAt: Date
+}
+
+// export default Vue.extend({
+//   data() {
+//     return {
+//       isRecording: false,
+//       todos: [],
+//     }
+//   },
+//   computed: {
+//     recButtonColor() {
+//       return this.isRecording ? 'pink' : 'gray'
+//     },
+//     webkitSpeechRecognition() {
+//       const { webkitSpeechRecognition }: IWindow = <IWindow>(<unknown>window)
+//       return new webkitSpeechRecognition()
+//     },
+//   },
+//   created() {
+//     const rec = this.webkitSpeechRecognition
+//     rec.continuous = false // 後述
+//     rec.interimResults = false // trueにすると認識途中の結果も返す
+//     rec.lang = 'ja-JP' // 言語を指定する
+//     rec.maxAlternatives = 1 // 結果候補の最大数(デフォルトは1)
+//     rec.onresult = (e: any) => {
+//       // 認識結果がSpeechRecognitionEventインスタンスとして渡されます
+//       // 認識した言葉を表示
+//       console.log(e.results[0][0].transcript)
+//       this.todos.push(new Todo(e.results[0][0].transcript))
+//       rec.stop() // 認識が完了したら終了する
+//     }
+
+//     rec.onaudiostart = () => {
+//       this.isRecording = true
+//     }
+//     rec.onaudioend = () => {
+//       this.isRecording = false
+//     }
+//   },
+//   methods: {
+//     recognizeVoice() {
+//       const rec = this.webkitSpeechRecognition
+//       if (this.isRecording) {
+//         this.isRecording = false
+//         rec.stop()
+//       } else {
+//         rec.start() // 認識を開始します
+//       }
+//     },
+//     removeTodo(todoId: string) {
+//       this.todos = this.todos.filter((t: Todo) => t.id !== todoId)
+//     },
+//   },
+// })
 </script>
 <style scoped>
 .todo-title {
